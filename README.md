d2bc0fa4d1c565101d3cd18c1b88af6ffd5e082f
Patrice Cadiot <pathibul.r@gmail.com>
2018-03-16 11:16:53 +0100

    crée des tests avec hspec et un mini README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # words
+In a grid full with letters, this little program find all the words
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,19 @@ import Data
 
 main :: IO ()
 main = hspec $ do
-  describe "How to write a test" $ do
-    it "Should be able to run tests" $ do
-      someString `shouldBe` "someString"
+  describe "formatGrid" $ do
+    it "Should concatenate every line with a new line" $ do
+      (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
+
+  describe "findWord" $ do
+    it "Should find words that exist on the Grid" $ do
+      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+      findWord grid "PERL" `shouldBe` Just "PERL"
+    it "Should not find words that don't exist on the Grid" $ do
+      findWord grid "HAMSTER" `shouldBe` Nothing
+
+  describe "findWords" $ do
+    it "Should find all the words that exist in the Grid" $ do
+      findWords grid languages `shouldBe` languages
+    it "Should not find the words that do not exist in the Grid" $ do
+      findWords grid ["GERMAN", "FRENCH", "ENGLISH"] `shouldBe` []
